43a256faf659b56d6c60ab25780d8709df63f903
cristianaquino <ycristian10@gmail.com>
2026-07-26 22:43:41 -0500

    feat: seed assign rol to user

---
+++ src/main/resources/db/seed/V33__insert_rol_user_procedure.sql
@@ -0,0 +1,19 @@
+DO $$
+DECLARE
+    v_user pos_system_users%rowtype;
+    v_contador INTEGER:=0;
+BEGIN
+    FOR v_user IN
+        SELECT *
+        FROM pos_system_users
+        ORDER BY RANDOM()
+    LOOP
+        v_contador:=v_contador+1;
+
+        IF v_contador = 1 THEN
+            INSERT INTO pos_system_user_role(user_id,role_id) VALUES(v_user.id,1);
+        ELSE
+            INSERT INTO pos_system_user_role(user_id,role_id) VALUES(v_user.id,2);
+        END IF;
+    END LOOP;
+END $$;
