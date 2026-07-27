7bb226decc4821506d864d5b974d8a0bbc0ab0bc
cristianaquino <ycristian10@gmail.com>
2026-07-26 22:38:46 -0500

    feat: seed data user

---
+++ src/main/resources/db/seed/V31__insert_data_users_table.sql
@@ -0,0 +1,48 @@
+INSERT INTO pos_system_users (
+    name,
+    father_last_name,
+    mother_last_name,
+    full_name,
+    username,
+    password
+) VALUES
+(
+    'Juan Carlos',
+    'Perez',
+    'Gomez',
+    'Juan Carlos Perez Gomez',
+    'C_PERJUA',
+    '$2a$12$yCSvzBCC.9dquqplkAPVnOHmYXLFMALmsy25oswaykSgAujKci2ku'
+),
+(
+    'Maria Fernanda',
+    'Lopez',
+    'Torres',
+    'Maria Fernanda Lopez Torres',
+    'C_LOPMAR',
+    '$2a$12$yCSvzBCC.9dquqplkAPVnOHmYXLFMALmsy25oswaykSgAujKci2ku'
+),
+(
+    'Carlos Alberto',
+    'Ramirez',
+    'Diaz',
+    'Carlos Alberto Ramirez Diaz',
+    'C_RAMCAR',
+    '$2a$12$yCSvzBCC.9dquqplkAPVnOHmYXLFMALmsy25oswaykSgAujKci2ku'
+),
+(
+    'Ana Lucia',
+    'Vargas',
+    'Soto',
+    'Ana Lucia Vargas Soto',
+    'C_VARANA',
+    '$2a$12$yCSvzBCC.9dquqplkAPVnOHmYXLFMALmsy25oswaykSgAujKci2ku'
+),
+(
+    'Sofia Isabel',
+    'Gutierrez',
+    'Rojas',
+    'Sofia Isabel Gutierrez Rojas',
+    'C_GUTSOF',
+    '$2a$12$yCSvzBCC.9dquqplkAPVnOHmYXLFMALmsy25oswaykSgAujKci2ku'
+);
