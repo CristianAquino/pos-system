7a91e7e764afe2ccc71f5bc938f5bde762bd647b
cristianaquino <ycristian10@gmail.com>
2026-07-26 22:39:09 -0500

    feat: seed data products

---
+++ src/main/resources/db/seed/V32__insert_data_products_table.sql
@@ -0,0 +1,189 @@
+INSERT INTO pos_system_products (
+    name,
+    description,
+    sale_price,
+    stock,
+    minimum_stock,
+    sku,
+    upc
+) VALUES
+(
+    'Arroz Extra 1Kg',
+    'Arroz extra calidad superior bolsa de 1 kilogramo',
+    5.50,
+    120,
+    20,
+    'SKU000000001',
+    '7751234567001'
+),
+(
+    'Azucar Rubia 1Kg',
+    'Azucar rubia embolsada de 1 kilogramo',
+    4.20,
+    85,
+    15,
+    'SKU000000002',
+    '7751234567002'
+),
+(
+    'Leche Evaporada',
+    'Leche evaporada lata de 400 gramos',
+    4.80,
+    150,
+    30,
+    'SKU000000003',
+    '7751234567003'
+),
+(
+    'Aceite Vegetal 1L',
+    'Aceite vegetal botella de 1 litro',
+    9.90,
+    45,
+    10,
+    'SKU000000004',
+    '7751234567004'
+),
+(
+    'Fideos Spaghetti 500g',
+    'Pasta tipo spaghetti paquete de 500 gramos',
+    3.50,
+    70,
+    15,
+    'SKU000000005',
+    '7751234567005'
+),
+(
+    'Gaseosa Cola 3L',
+    'Bebida gaseosa sabor cola botella de 3 litros',
+    12.50,
+    25,
+    8,
+    'SKU000000006',
+    '7751234567006'
+),
+(
+    'Agua Mineral 625ml',
+    'Agua mineral sin gas botella de 625 mililitros',
+    2.00,
+    200,
+    30,
+    'SKU000000007',
+    '7751234567007'
+),
+(
+    'Jabon de Tocador',
+    'Jabon de tocador aroma fresco',
+    2.80,
+    60,
+    10,
+    'SKU000000008',
+    '7751234567008'
+),
+(
+    'Detergente 800g',
+    'Detergente en polvo bolsa de 800 gramos',
+    8.50,
+    18,
+    10,
+    'SKU000000009',
+    '7751234567009'
+),
+(
+    'Papel Higienico x4',
+    'Paquete de papel higienico de 4 unidades',
+    6.90,
+    35,
+    12,
+    'SKU000000010',
+    '7751234567010'
+),
+(
+    'Cafe Instantaneo 200g',
+    'Cafe instantaneo frasco de 200 gramos',
+    14.90,
+    12,
+    5,
+    'SKU000000011',
+    '7751234567011'
+),
+(
+    'Chocolate en Barra',
+    'Chocolate de leche barra de 100 gramos',
+    3.20,
+    90,
+    20,
+    'SKU000000012',
+    '7751234567012'
+),
+(
+    'Atun en Lata',
+    'Atun en aceite lata de 170 gramos',
+    6.50,
+    55,
+    10,
+    'SKU000000013',
+    '7751234567013'
+),
+(
+    'Galletas de Vainilla',
+    'Paquete de galletas sabor vainilla',
+    2.50,
+    100,
+    20,
+    'SKU000000014',
+    '7751234567014'
+),
+(
+    'Shampoo 400ml',
+    'Shampoo para todo tipo de cabello',
+    15.90,
+    8,
+    5,
+    'SKU000000015',
+    '7751234567015'
+),
+(
+    'Cepillo Dental',
+    'Cepillo dental de cerdas suaves',
+    4.50,
+    40,
+    10,
+    'SKU000000016',
+    '7751234567016'
+),
+(
+    'Desinfectante 1L',
+    'Desinfectante multiusos botella de 1 litro',
+    7.90,
+    22,
+    10,
+    'SKU000000017',
+    '7751234567017'
+),
+(
+    'Harina 1Kg',
+    'Harina preparada bolsa de 1 kilogramo',
+    4.30,
+    65,
+    15,
+    'SKU000000018',
+    '7751234567018'
+),
+(
+    'Sal de Mesa 1Kg',
+    'Sal refinada bolsa de 1 kilogramo',
+    2.10,
+    95,
+    20,
+    'SKU000000019',
+    '7751234567019'
+),
+(
+    'Fresas congeladas 1Kg',
+    'Fresas congeladas bolsa de 1 kilogramo',
+    10.00,
+    20,
+    5,
+    'SKU000000020',
+    '7751234567020'
+);
