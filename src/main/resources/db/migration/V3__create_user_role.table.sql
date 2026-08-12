d408e727e0d8c4e7929905cf2dc54c37c6d96d84
cristianaquino <ycristian10@gmail.com>
2026-08-11 21:12:10 -0500

    feat: change in user_role table

--- src/main/resources/db/migration/V3__create_user_role.table.sql
+++ src/main/resources/db/migration/V3__create_user_role.table.sql
@@ -1,7 +1,6 @@
 CREATE TABLE pos_system_user_role (
     user_id UUID NOT NULL,
     role_id BIGINT NOT NULL,
-    assigned_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY(user_id,role_id)
 );
@@ -14,7 +13,4 @@ COMMENT ON COLUMN pos_system_user_role.user_id IS
 
 COMMENT ON COLUMN pos_system_user_role.role_id IS
 'Clave foranea del rol';
-
-COMMENT ON COLUMN pos_system_user_role.assigned_at IS
-'Fecha que se realiza la asignacion del rol al usuario';
 
