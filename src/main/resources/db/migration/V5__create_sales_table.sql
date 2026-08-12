cabf597dc834f86805e705babeff24e2079a70c8
cristianaquino <ycristian10@gmail.com>
2026-08-11 21:12:32 -0500

    feat: change in sales table

--- src/main/resources/db/migration/V5__create_sales_table.sql
+++ src/main/resources/db/migration/V5__create_sales_table.sql
@@ -2,13 +2,13 @@ CREATE TABLE pos_system_sales (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     user_id UUID NOT NULL,
     cash_opening_id UUID NOT NULL,
-    payment_method_id INTEGER NOT NULL,
+    payment_method_id BIGINT NOT NULL,
     sale_code VARCHAR(200) DEFAULT NULL,
     subtotal NUMERIC(10,2) NOT NULL DEFAULT 0 CHECK(subtotal>=0),
-    tax INTEGER NOT NULL DEFAULT 0 CHECK(tax>=0),
-    discount INTEGER NOT NULL DEFAULT 0 CHECK(discount>=0),
+    ptax INTEGER NOT NULL DEFAULT 0 CHECK(ptax>=0),
+    tax NUMERIC(10,2) NOT NULL DEFAULT 0 CHECK(tax>=0),
     total NUMERIC(10,2) NOT NULL DEFAULT 0 CHECK(total>=0),
-    status CHAR(1) NOT NULL DEFAULT 'A' CHECK (status IN ('A','I')),
+    status VARCHAR(1) NOT NULL DEFAULT 'A' CHECK(status IN ('A','I')),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT NULL
 );
@@ -34,14 +34,14 @@ COMMENT ON COLUMN pos_system_sales.sale_code IS
 COMMENT ON COLUMN pos_system_sales.subtotal IS
 'Subtotal de la venta realizada';
 
-COMMENT ON COLUMN pos_system_sales.tax IS
-'Porcentaje de impuesto a la venta, valor debe estar en entero';
-
-COMMENT ON COLUMN pos_system_sales.discount IS
+COMMENT ON COLUMN pos_system_sales.ptax IS
 'Porcentaje de descuento a la venta, valor debe estar en entero';
 
+COMMENT ON COLUMN pos_system_sales.tax IS
+'Total a pagar por impuestos';
+
 COMMENT ON COLUMN pos_system_sales.total IS
-'Total a pagar por la venta, incluye subtotal + tax - discount';
+'Total a pagar por la venta, incluye subtotal + tax';
 
 COMMENT ON COLUMN pos_system_sales.status IS
 'Estado de la venta, valores permitidos son A = activo, I = anulada';
