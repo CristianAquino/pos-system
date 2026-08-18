ad4d9579eb85c155a438659de117207e522c7581
cristianaquino <ycristian10@gmail.com>
2026-08-17 19:20:24 -0500

    feat: role response dto

--- src/main/java/cris_dev/pos_system/Role/Model/DTO/Response/RoleResponse.java
+++ src/main/java/cris_dev/pos_system/Role/Model/DTO/Response/RoleResponse.java
@@ -1,15 +1,8 @@
 package cris_dev.pos_system.Role.Model.DTO.Response;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class RoleResponse {
-    private Long id;
-    private String code;
-    private String description;
-    private String status;
+public record RoleResponse(
+        Long id,
+        String code,
+        String description,
+        String status) {
 }
