dd58d13a2cdc3ff2645318fc7ff3128f9fe1c4d2
cristianaquino <ycristian10@gmail.com>
2026-08-17 19:20:37 -0500

    feat: role controller

--- src/main/java/cris_dev/pos_system/Role/Controller/RoleController.java
+++ src/main/java/cris_dev/pos_system/Role/Controller/RoleController.java
@@ -3,10 +3,11 @@ package cris_dev.pos_system.Role.Controller;
 import cris_dev.pos_system.Role.Model.DTO.Request.RoleCreateRequest;
 import cris_dev.pos_system.Role.Model.DTO.Request.RoleUpdateStatusRequest;
 import cris_dev.pos_system.Role.Model.DTO.Response.RoleResponse;
-import cris_dev.pos_system.Role.Service.IRoleService;
+import cris_dev.pos_system.Role.Service.RoleService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -19,33 +20,38 @@ import java.util.List;
 @RequestMapping("role")
 public class RoleController {
 
-    private final IRoleService iRoleService;
+    private final RoleService roleService;
 
-    @GetMapping("/all")
+    @GetMapping("all")
     public ResponseEntity<List<RoleResponse>> allRoles(
-            @RequestParam(required = false) @Pattern(regexp = "^(A|I)$") String status
-                                                      ) {
-        List<RoleResponse> roles = iRoleService.allRoles(status);
-        return ResponseEntity.status(200).body(roles);
+            @RequestParam(required = false) @Pattern(regexp = "^[aiAI]$") String status) {
+        List<RoleResponse> roles = roleService.allRoles(status);
+        return ResponseEntity.status(HttpStatus.OK).body(roles);
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<String> createRole(
-            @RequestBody @Valid RoleCreateRequest req
-                                            ) {
-        String message = iRoleService.createRole(req);
-        return ResponseEntity.ok().body(message);
+    @GetMapping("soft_delete/all")
+    public ResponseEntity<List<RoleResponse>> allRolesSoftDelete() {
+        List<RoleResponse> roles = roleService.allRolesSoftDelete();
+        return ResponseEntity.status(HttpStatus.OK).body(roles);
     }
 
-    @PutMapping("/update/status")
-    public ResponseEntity<String> updatedStatus(
-            @RequestBody @Valid RoleUpdateStatusRequest req
-                                               ) {
-        try {
-            String message = iRoleService.updStatusRole(req);
-            return ResponseEntity.accepted().body(message);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    @PostMapping("create")
+    public ResponseEntity<RoleResponse> createRole(
+            @RequestBody @Valid RoleCreateRequest payload) {
+        RoleResponse role = roleService.createRole(payload);
+        return ResponseEntity.status(HttpStatus.OK).body(role);
+    }
+
+    @PutMapping("update/status")
+    public ResponseEntity<RoleResponse> updatedStatus(
+            @RequestBody @Valid RoleUpdateStatusRequest req) {
+        RoleResponse role = roleService.updStatusRole(req);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(role);
+    }
+
+    @PutMapping("soft_delete/{id}")
+    public ResponseEntity<String> updatedSoftDelete(@PathVariable Long id) {
+        String msg = roleService.softDeleteRole(id);
+        return ResponseEntity.status(HttpStatus.OK).body(msg);
     }
 }
