0b512d43fd79339596099cf09cd14431fcf7ade6
cristianaquino <ycristian10@gmail.com>
2026-08-19 11:30:28 -0500

    refactor: create query jdbc for search product

--- src/main/java/cris_dev/pos_system/Product/Repository/ProductRepository.java
+++ src/main/java/cris_dev/pos_system/Product/Repository/ProductRepository.java
@@ -4,6 +4,8 @@ import cris_dev.pos_system.Product.Model.Entity.ProductEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +13,18 @@ import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    @Query("""
+                SELECT p
+                FROM ProductEntity p
+                WHERE (:sku IS NULL OR p.sku = :sku )
+                AND (:upc IS NULL OR p.upc = :upc)
+                AND (:sName IS NULL OR p.searchName LIKE :sName)
+            """)
+    public List<ProductEntity> searchProducts(
+            @Param("sku") String sku,
+            @Param("upc") String upc,
+            @Param("sName") String sName);
+
     public Optional<ProductEntity> findBySlug(String slug);
 
     public List<ProductEntity> findBySlugIsNull();
